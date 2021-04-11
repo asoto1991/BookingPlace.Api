35d77c3c210e4e2f8bedb605bc2cd18de2ae660f
Augusto Soto <arcangel_2_1@hotmail.com>
2021-04-11 14:02:04 -0400

    commit

--- BookingPlace.Api/Controllers/WeatherForecastController.cs
+++ BookingPlace.Api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace BookingPlace.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
